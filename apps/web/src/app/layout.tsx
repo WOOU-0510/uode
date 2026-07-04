b8300ccdff1f20b55095f3495a91feeda8a22a23
wooyunha <wjh0970@gmail.com>
2026-07-05 05:33:07 +0900

    chore

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,9 +1,9 @@
-import { geistMono, geistSans } from "@/shared/assets/fonts";
 import { GlobalPopoversLayout } from "@/widgets/popover/escape";
 import cn from "classnames";
 import type * as React from "react";
 import "@/shared/styles/globals.scss";
 import "@uode/base-ui-react/style.css";
+import { geistMono, geistSans } from "@/shared/assets/fonts";
 
 type RootLayoutProps = {
   children: React.ReactNode;
